d370a711fcc667db54cd2c6a34b116a8b484bc8b
David <daviddidolla@gmail.com>
2023-09-20 12:15:05 +0200

    master

---
+++ Exo/memory/script.js
@@ -0,0 +1,58 @@
+var nbPair;
+var nbPlayer;
+
+
+/**
+ * initialise la partie selon les parametre
+ * @param {*} nbPair 
+ * @param {*} nbPlayer 
+ */
+function initGame(nbPair,nbPlayer) {
+    
+}
+
+/**
+ * affiche le nombre de paire de carte 
+ * @param {*} nbPair 
+ */
+function generateCards(nbPair) {
+    
+}
+
+/**
+ * affiche le recto de la carte cliquée
+ */
+function flipCard() {
+    
+}
+
+/**
+ * affiche le score lors d'un changement dans le tableau des score
+ */
+function displayUpdatedScore() {
+    
+}
+
+/**
+ * 
+ * 
+ */
+function displayNextPlayer() {
+    
+}
+
+
+/**
+ * 
+ * @returns bool
+ */
+function checkPair() {
+    
+}
+
+/**
+ * 
+ */
+function triggerWin() {
+    
+}
